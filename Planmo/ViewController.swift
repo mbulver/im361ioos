526981a8b4c6afae7e51d837b42cd9451914a431
Max Bulver <mbulver@mail.bradley.edu>
2019-04-09 22:14:50 -0500

    Initial Commit

---
+++ Planmo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planmo
+//
+//  Created by Maxwell Bulver on 4/9/19.
+//  Copyright © 2019 Maxwell Bulver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
