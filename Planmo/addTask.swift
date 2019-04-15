526aa26deed9fae5fb7a3855048d63bd0933c8ad
Max Bulver <mbulver@mail.bradley.edu>
2019-04-15 18:51:05 -0500

    class

---
+++ Planmo/addTask.swift
@@ -0,0 +1,19 @@
+//
+//  addTask.swift
+//  Planmo
+//
+//  Created by Maxwell Bulver on 4/15/19.
+//  Copyright © 2019 Maxwell Bulver. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class addTask: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func didPress(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
