c2097e34367f19bd3ea9ccb1fff15c938d5aa98e
Max Bulver <mbulver@mail.bradley.edu>
2019-04-16 22:16:42 -0500

    image and alert

--- Planmo/ViewController.swift
+++ Planmo/ViewController.swift
@@ -9,53 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var Button: UIButton!
-    var isSegueAllowed = false
+    @IBOutlet weak var addtaskbutton: UIButton!
     
-    @IBAction func didPressSwitch(_ sender: Any) {
-        isSegueAllowed = !isSegueAllowed
-    }
-    @IBAction func didPressAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello World!", message: "This is an aler", preferredStyle: .actionSheet)
-        let action1 = UIAlertAction(title: "Hello", style: .default){_ in
-            print("Hello was clicked")
-        }
-        let action2 = UIAlertAction(title: "Cancel", style: .cancel){_ in
-            print("action was canceled")
-        }
-        let action3 = UIAlertAction(title: "DELETE", style: .destructive){_ in
-            print("DELETE WAS CLICKED")
-        }
-        alert.addAction(action1)
-        alert.addAction(action2)
-        alert.addAction(action3)
-        
-        present(alert,animated:true){
-            print("alert was shown")
-        }
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        func shouldPerformSegue(withIdentifier indentifier: String, sneder: Any?)-> Bool{
-            switch indentifier {
-            case "addTask":
-                print("going to add task")
-              
-                if isSegueAllowed{
-                    return true
-                } else {
-                    return false
-                }
-                
-            default:
-                print("I don't know what's happening")
-                return true
-            }
-        }
         // Do any additional setup after loading the view.
+        }
     }
-
-
-}
 
--- Planmo/addTask.swift
+++ Planmo/addTask.swift
@@ -10,10 +10,33 @@ import Foundation
 import UIKit
 
 class addTask: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func didPress(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    
+    @IBAction func didAddTask(_ sender: Any) {
+        let alert = UIAlertController(title: "Create Task", message: "Add this task to your schedule?", preferredStyle: .alert)
+        
+
+        // create a default action
+        let action1 = UIAlertAction(title: "Add", style: .default) { _ in
+            // this code will be run when the user picks "Hello"
+            print("Add was clicked")
+        }
+        // create a cancel action
+        let action2 = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            // this code will be run when the user picks "Cancel"
+            print("action was cancelled")
+        }
+        alert.addAction(action1)
+        alert.addAction(action2)
+        
+        present(alert,
+                animated: true) {
+                    // this code will run after the alert is shown
+                    print("alert was shown")
     }
+    
+}
 }
